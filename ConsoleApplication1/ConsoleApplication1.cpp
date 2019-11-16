e8c91609ef35d0ab310eea7a2cf2449552a57322
E-lope <ileilliat@gmail.com>
2019-11-16 17:19:57 +0900

    Basic Score: new and delete

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,138 +1,81 @@
-﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <random>
+#include <set>
+#include <cctype>
 
 using namespace std;
 
-class Person {
-	string lastName, firstName;
-	int age;
-public:
-	Person(const string& first, const string& last, int age = 1)
-		:lastName(last), firstName(first) {
-		this->age = age;
-	}
-	~Person() {}//소멸자
-	void setName(const string& lastName) {
-		this->lastName = lastName;
-	}
-	void setName(const string& firstName, const string& lastName) {
-		this->firstName = firstName;
-		this->lastName = lastName;
-	}
-	string getName() const { 
-		return lastName + ", " + firstName; 
-	}
-	virtual void print() const {
-		cout << lastName << ", " << firstName << ", age of " << age << endl;
-	}
-};
-
-class Student : public Person {
-	string schoolName;
-	int year;
-public:
-	Student(const string& firstName, const string& lastName,
-		const string& schoolName, int year = 1)
-		: Person(firstName, lastName, year + 19), schoolName(schoolName) {
-		this->year = year;
-	}
-	int getYear() const { return year; }
-	void print() const {
-		cout << getName() << " is " << year << " grader in "
-			<< schoolName << endl;
-	}
-};
-
-class Complex {
+class Score {
 private:
-	double re, im;
+	int* list;
+	int size;
+	int pos;
 public:
-	Complex(double re = 0, double im = 0) {
-		this->re = re;
-		this->im = im;
+	Score(int size): size(size), pos(0) {
+		list = new int[size];
 	}
 
-	Complex operator+ (const Complex& c) const {
-		return Complex(re + c.re, im + c.im);
+	~Score() {
+		delete[] list;
 	}
 
-	double getRe() const { return re; }
-	double getIm() const { return im; }
+	bool add(const int value) {
+		if (pos > size - 1) {
+			return false;
+		} else {
+			list[pos] = value;
+			pos++;
+			return true;
+		}
+	}
+
+	int sum() {
+		int result = 0;
+		for (int i = 0; i < pos; ++i) {
+			result += list[i];
+		}
+		return result;
+	}
+
+	double average() {
+		return static_cast<double>(sum()) / pos;
+	}
+
+	void print(const string& command) {
+		if (command == "add") {
+			cout << "Enter Score: ";
+			int value;
+			cin >> value;
+			if (value < 0) {
+				cout << "System: " << value << "can't be score" << endl;
+			} else if (add(value)) {
+				cout << "System: " << value << " added." << endl;
+			} else {
+				cout << "System: Too many score" << endl;
+			}
+		} else if (command == "sum") {
+			cout << "System: Sum: " << sum() << endl;
+		} else if (command == "average") {
+			if (pos == 0)
+				cout << "1개 이상의 점수가 입력되어야 합니다." << endl;
+			else
+				cout << "System: Avg: " << average() << endl;
+		}
+	}
 };
-ostream& operator << (ostream& os, const Complex& c) {
-	os << c.getRe() << " + " << c.getIm() << "i";
-	return os;
-}
-
-template <class T>
-T myMax(T int1, T int2) {
-	if (int1 > int2) return int1;
-	return int2;
-}
-template <class T>
-T findMax(T numbers[], int size) {
-	T maxValue = numbers[0];
-	for (int i = 1; i < size; i++)
-		maxValue = myMax(maxValue, numbers[i]);
-	return maxValue;
-}
-
-const int SIZE = 200;
-template <class T>
-class Stack {
-	T elems[SIZE];
-	int top;
-public:
-	Stack() { top = 0; }
-	void push(const T& elem) { elems[top++] = elem; }
-	T pop() { return elems[--top]; }
-	bool isEmpty() const { return top == 0; }
-};
-
-const char* const E_NON_POSITIVE_LENGTH = "Non positive length";
-const char* const E_NON_TRIANGLE = "Not a triangle";
-
-int getTriangleLength(int x, int y, int z) {
-	if (x <= 0 || y <= 0 || z <= 0) throw E_NON_POSITIVE_LENGTH;
-	if (x + y <= z || x + z <= y || y + z <= x) throw E_NON_TRIANGLE;
-	return x + y + z;
-
-}
-
-namespace NS1 {
-	int value;
-}
-namespace NS2 {
-	int value;
-}
 
 int main() {
-	Student hong("kildong", "Hong", "HK", 5);
-	cout << hong.getName() << endl;
-	hong.print();
-
-	Stack<int> is;
-	is.push(100);
-	cout << is.pop() << endl;
-
-	Stack<Complex> cos;
-	cos.push(Complex(10, 10));
-	cos.push(Complex(20, 10));
-	cout << cos.pop() << endl;
-
-	int x, y, z;
-	cin >> x >> y >> z;
-	try {
-		int totalLength = getTriangleLength(x, y, z);
-		cout << totalLength << endl;
+	cout << "Enter the score count: ";
+	int maxSize;
+	cin >> maxSize;
+	Score score(maxSize);
+	string command;
+	while (command != "quit") {
+		cout << "Enter Command: (add, sum, average, quit) ";
+		cin >> command;
+		for (char& i : command) {
+			i = std::tolower(i);
+		}
+		score.print(command);
 	}
-	catch (const char* const e) {
-		cout << e << endl;
-	}
-
-	NS1::value = 100;
-	NS2::value = 200;
-
 }
